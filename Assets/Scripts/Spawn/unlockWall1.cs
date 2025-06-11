878fe2151eb3f46880e5034ab33576c60f5bf003
paracodina <montuoriLDev@gmail.com>
2025-06-12 01:45:18 +0200

    [SCENE] Key and Heal Items added

---
+++ Assets/Scripts/Spawn/unlockWall1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unlockWall1 : MonoBehaviour
+{
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

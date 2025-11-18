4112538a44672a158579c29b01a7c674a71fc712
paracodina <montuoriLDev@gmail.com>
2025-11-18 23:11:48 +0100

    [SCRIPT][UI] Nicknames' logic created
    Implementata la logica di scelta del nickname, con tanto di modifiche all'UI

---
+++ Assets/Scripts/NicknameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NicknameManager : MonoBehaviour
+{
+    public GameObject nicknamePanel;
+    public TMP_InputField nicknameInputField;
+    public TMP_Text nicknameDisplayText;
+
+    private const string NicknameKey = "PlayerNickname";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(NicknameKey))
+        {
+            string savedName = PlayerPrefs.GetString(NicknameKey);
+            nicknameDisplayText.text = savedName;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ConfirmNickname()
+    {
+        string nickname = nicknameInputField.text;
+        if (string.IsNullOrEmpty(nickname))
+            return;
+
+        PlayerPrefs.SetString(NicknameKey, nickname); // per salvare il nickname
+
+        nicknameDisplayText.text = nickname;
+
+        PlayerPrefs.Save(); // flush per sicurezza 
+
+        nicknamePanel.SetActive(false);
+    }
+}
